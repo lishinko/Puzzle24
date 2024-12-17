6ee3e73fc06a0d56f4a17fe231e78d3bd9abacb8
xinggang.li <xinggang.li@horizon.cc>
2024-12-17 19:58:17 +0800

    z转换器实例。实际上是不需要的

---
+++ Puzzle24/StringToIntConverter.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle24
+{
+    /// <summary>
+    /// 转换器实例。实际上，string to int是自动转换的，弄个转换器反而出错
+    /// </summary>
+    public class StringToIntConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is string source)
+            {
+                return int.Parse(source);
+            }
+            // converter used for the wrong type
+            return new BindingNotification(new InvalidCastException(),
+                                                    BindingErrorType.Error);
+        }
+
+        /// <summary>
+        /// 实际上是不需要的，直接throw not supported就可以。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is int source)
+            {
+                return source.ToString();
+            }
+            // converter used for the wrong type
+            return new BindingNotification(new InvalidCastException(),
+                                                    BindingErrorType.Error);
+        }
+    }
+}
--- Puzzle24/ViewModels/MainViewModel.cs
+++ Puzzle24/ViewModels/MainViewModel.cs
@@ -9,22 +9,22 @@ public partial class MainViewModel : ViewModelBase
     [ObservableProperty]
     private string _result = "还没有计算出来";
     [ObservableProperty]
-    private string _data0 = "3";
+    private int _data0 = 3;
     [ObservableProperty]
-    private string _data1 = "8";
+    private int _data1 = 8;
     [ObservableProperty]
-    private string _data2 = "8";
+    private int _data2 = 8;
     [ObservableProperty]
-    private string _data3 = "8";
+    private int _data3 = 8;
     public void RunSolver()
     {
         try
         {
             var items = new int[4];
-            items[0] = int.Parse(Data0);
-            items[1] = int.Parse(Data1);
-            items[2] = int.Parse(Data2);
-            items[3] = int.Parse(Data3);
+            items[0] = Data0;
+            items[1] = Data1;
+            items[2] = Data2;
+            items[3] = Data3;
             var p = new PuzzleSolver(items);
             if (p.Solve24(out var result, out var ops))
             {
